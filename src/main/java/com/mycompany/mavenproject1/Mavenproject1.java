93a40013bdcbe1a72b8df39cab736df9d33aecde
Ceda135 <cdherrera2005@gmail.com>
2025-04-25 10:03:16 -0500

    Hola mundo

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -1,16 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mavenproject1;
 
-/**
- *
- * @author César
- */
-public class Mavenproject1 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Mavenproject1
+{
+
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingrese el primer nombre");
+        String nombre1 = scanner.nextLine();
+        
+        System.out.print("Ingrese el segundo nombre");
+        String nombre2 = scanner.nextLine();
+        
+        System.out.print("Ingrese el primer apellido");
+        String apellido1 = scanner.nextLine();
+        
+        System.out.print("Ingrese el segundo apellido");
+        String apellido2 = scanner.nextLine();
+        
+        String nombreCompleto = nombre1 + " " + nombre2 + " " + apellido1 + " " + apellido2;
+        
+        System.out.println("Nombre completo: " + nombreCompleto);
+        
+        scanner.close();
     }
 }
